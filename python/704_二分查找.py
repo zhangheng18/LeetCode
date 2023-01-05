7676cb816c4389011b650d7e2255a0d4f42e1818
xiaoTaoist <taoist@protonmail.com>
2023-01-05 19:42:04 +0800

    feat:add 704

---
+++ python/704_二分查找.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) -1
+        while left <= right:
+            mid = left + int(( right - left) /2)
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                left = mid+1
+            elif target < nums[mid]:
+                right = mid-1
+        return -1
