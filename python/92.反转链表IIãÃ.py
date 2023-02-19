18dc8075ea545a057480a72c10a09dfa47c1e140
xiaoTaoist <taoist@protonmail.com>
2023-02-19 22:06:46 +0800

    feat: add 92 py

---
+++ python/92.反转链表IIãÃ.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def __init__(self):
+        self.n_next = None
+
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == 1:
+            return self.reverseN(head, right)
+        head.next = self.reverseBetween(head.next , left-1, right-1)
+        return head
+
+    
+    def reverseN(self, head, n):
+        if n == 1:
+            self.n_next = head.next
+            return head
+        last = self.reverseN(head.next, n-1)
+        head.next.next = head
+        head.next = self.n_next
+        return last        
+
