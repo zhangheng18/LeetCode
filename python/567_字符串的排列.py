548e85f5db73b1f346ef4632d3881e996a781494
xiaoTaoist <taoist@protonmail.com>
2023-04-25 19:43:32 +0800

    fix: update 567 py

--- python/567_字符串的排列.py
+++ python/567_字符串的排列.py
@@ -1,31 +1,36 @@
+from collections import defaultdict
+
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        left, right = 0,0
-        windows = {i:0 for i in s1}
-        need = {}
-
+        # 记录字符需要出现的次数
+        need, windows = defaultdict(int), defaultdict(int)
         for i in s1:
-            need.setdefault(i, 0)
-            need[i] +=1
+            need[i] += 1
 
-
-
+        left, right = 0, 0
         valid = 0
+
+        # 扩大字符串
         while right < len(s2):
-            c= s2[right]
-            right +=1
+            c = s2[right]
+            right += 1
             if c in need:
-                windows[c] +=1
-                if need[c] == windows[c]:
-                    valid +=1
-            
-            while (right - left) >= len(s1):
+                #更新窗口
+                windows[c] += 1
+                if windows[c] == need[c]:
+                    valid += 1
+
+            # 收缩字符串
+            while right - left >= len(s1):
                 if valid == len(need):
                     return True
-                d = s2[left]
-                left +=1
-                if d in need:
-                    if windows[d] == need[d]:
+                c = s2[left]
+                left += 1
+                if c in need:
+                    #更新窗口
+                    if windows[c] == need[c]:
                         valid -= 1
-                    windows[d] -= 1
+                    windows[c] -= 1
+        # 未找到
         return False
