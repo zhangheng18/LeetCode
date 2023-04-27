b128e97d554d9dffe4478e5a1081f6cc307e1d07
xiaoTaoist <taoist@protonmail.com>
2023-04-27 22:18:23 +0800

    fix: update 3

--- python/3_无重复字符的最长子串.py
+++ python/3_无重复字符的最长子串.py
@@ -1,17 +1,25 @@
+from collections import defaultdict
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left, right = 0,0
-        windows = {}
+        left, right = 0, 0
+        windows = defaultdict(int)
 
         res = 0
-        while right < len(s):
-            c = s[right]
-            right +=1
-            windows[c] = windows.get(c,0) +1
 
+        while right < len(s):
+            # 扩大窗口
+            c = s[right]
+            windows[c] += 1
+            right += 1
+
+            # 存在重复字符，缩小窗口
             while windows[c] > 1:
-                d = s[left]
-                left +=1
-                windows[d] -=1
+                windows[s[left]] -= 1
+                left += 1
+
+            # 更新结果
             res = max(res, right-left)
+
         return res
