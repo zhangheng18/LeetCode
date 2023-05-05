bf1705b0b465a34e6a589a030a1ca7564b099efa
xiaoTaoist <taoist@protonmail.com>
2023-05-05 22:17:35 +0800

    fix: update 34 py

--- python/34_在排序数组中查找元素的第一个和最后一个位置.py
+++ python/34_在排序数组中查找元素的第一个和最后一个位置.py
@@ -1,34 +1,40 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        return [ self.left_bound(nums, target), self.right_bound(nums, target)]
-    
+        return [self.left_bound(nums, target), self.right_bound(nums, target)]
+
     def left_bound(self, nums, target):
-        left = 0
-        right = len(nums) -1
+        left, right = 0, len(nums) - 1
+
+        # 搜索区间为 [left, right]
         while left <= right:
-            mid = left + ( right - left) // 2
-            if nums[mid] == target:
-                right = mid -1
-            elif nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] > target:
+            # 防止溢出
+            mid = left + (right - left) // 2
+            if target < nums[mid]:
                 right = mid - 1
-        if left == len(nums):
+            elif target > nums[mid]:
+                left = mid + 1
+            # 向左收缩
+            elif target == nums[mid]:
+                right = mid - 1
+        # 越界
+        if left == len(nums) or nums[left] != target:
             return -1
-        return left  if nums[left] == target  else -1
-    
+        return left
 
     def right_bound(self, nums, target):
-        left = 0
-        right = len(nums) -1
+        left, right = 0, len(nums) - 1
+        # 搜索区间为 [left, right]
         while left <= right:
             mid = left + (right - left) // 2
-            if target == nums[mid]:
-                left = mid +1
-            elif target < nums[mid]:
-                right = mid -1
+            if target < nums[mid]:
+                right = mid - 1
             elif target > nums[mid]:
-                left = mid +1
-        if left -1  < 0:
+                left = mid + 1
+            # 向右收缩
+            elif target == nums[mid]:
+                left = mid + 1
+        # 越界
+        if right < 0 or nums[right] != target:
             return -1
-        return left-1 if nums[left-1] == target else -1
+
+        return right
