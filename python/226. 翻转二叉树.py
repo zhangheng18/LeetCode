470336848744367b902dd0b0a5bf07b942950267
xiaoTaoist <taoist@protonmail.com>
2023-05-23 21:31:05 +0800

    feat: add 226 py

---
+++ python/226. 翻转二叉树.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        self.traverse(root)
+        return root
+
+    def traverse(self, root):
+        if root is None:
+            return
+
+        root.left, root.right = root.right, root.left
+        self.traverse(root.left)
+        self.traverse(root.right)
