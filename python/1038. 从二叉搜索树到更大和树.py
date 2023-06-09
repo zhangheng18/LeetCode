04631f886122c7ee1a536f4798c93bd074aa15c8
xiaoTaoist <taoist@protonmail.com>
2023-06-09 20:53:29 +0800

    feat: add 1038 py

---
+++ python/1038. 从二叉搜索树到更大和树.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        self.sum = 0
+
+        self.traverse(root)
+        return root
+
+    def traverse(self, root):
+        if root is None:
+            return
+        # 二叉树 逆中序遍历 （降序)
+        self.traverse(root.right)
+
+        # 记录累加和
+        self.sum += root.val
+        root.val = self.sum
+
+        self.traverse(root.left)
