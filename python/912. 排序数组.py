d5123377630957edc5d47af6fee515731c36781e
xiaoTaoist <taoist@protonmail.com>
2023-06-16 20:27:49 +0800

    fix: update 912 py

--- python/912. 排序数组.py
+++ python/912. 排序数组.py
@@ -1,43 +1,59 @@
+import random
+
+
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        self.sort(nums)
-        return nums
+        # self.merge_sort(nums)
+        return self.quick_sort(nums)
 
-    def sort(self, nums):
-        # 避免内存反复分配
-        self.temp = len(nums) * [0]
-        self._sort(nums, 0, len(nums) - 1)
-
-    def _sort(self, nums, lo, hi):
+    @staticmethod
+    def merge_sort(nums):
         # 归并排序
-        if lo == hi:
-            return
 
-        mid = (lo + hi) // 2
-        self._sort(nums, lo, mid)
-        self._sort(nums, mid + 1, hi)
-        self._merge(nums, lo, mid, hi)
+        # 初始化临时数组
+        temp = [0] * len(nums)
 
-    def _merge(self, nums, lo, mid, hi):
-        l_start, r_start = lo, mid + 1
+        def merge(nums, lo, mid, hi):
+            # 赋值 temp
+            temp[lo : hi + 1] = nums[lo : hi + 1]
 
-        temp = self.temp
-        for i in range(lo, hi + 1):
-            temp[i] = nums[i]
+            i, j = lo, mid + 1
+            for p in range(lo, hi + 1):
+                # 左边数组已经遍历完
+                if i > mid:
+                    nums[p] = temp[j]
+                    j += 1
+                # 右边数组已经遍历完
+                elif j > hi:
+                    nums[p] = temp[i]
+                    i += 1
+                # 左边数组的值大于右边数组的值 取右边数组的值
+                elif temp[i] > temp[j]:
+                    nums[p] = temp[j]
+                    j += 1
+                else:
+                    nums[p] = temp[i]
+                    i += 1
 
-        for i in range(lo, hi + 1):
-            # 左半数组合并完成
-            if l_start == mid + 1:
-                nums[i] = temp[r_start]
-                r_start += 1
-            # 右半数组合并完成
-            elif r_start == hi + 1:
-                nums[i] = temp[l_start]
-                l_start += 1
+        def sort(nums, lo, hi):
+            if lo == hi:
+                return
+            mid = (lo + hi) // 2
+            sort(nums, lo, mid)
+            sort(nums, mid + 1, hi)
+            merge(nums, lo, mid, hi)
 
-            elif temp[l_start] < temp[r_start]:
-                nums[i] = temp[l_start]
-                l_start += 1
-            else:
-                nums[i] = temp[r_start]
-                r_start += 1
+        sort(nums, 0, len(nums) - 1)
+
+    def quick_sort(self, nums):
+        # 快速排序
+        if len(nums) <= 1:
+            return nums
+
+        # 随机选择pivot 分区点
+        pivot = random.choice(nums)
+
+        # 三路快排， 避免相同值的元素过多导致的退化
+        left = self.quick_sort([x for x in nums if x < pivot])
+        right = self.quick_sort([x for x in nums if x > pivot])
+        return left + [x for x in nums if x == pivot] + right
