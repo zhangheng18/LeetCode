57c9155c3b03684ad115975e86c40295e3406e6b
xiaoTaoist <taoist@protonmail.com>
2023-06-30 22:13:44 +0800

    feat: add 146 py

---
+++ python/146. LRU 缓存.py
@@ -0,0 +1,29 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)  # 将key放到最后
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+            return
+
+        if len(self.cache) >= self.cap:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
