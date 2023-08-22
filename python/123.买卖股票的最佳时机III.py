6395a4aebe84b792c4baea225c475f6fbd6d422b
xiaoTaoist <taoist@protonmail.com>
2023-08-22 21:45:51 +0800

    fix: update 123 py

--- python/123.买卖股票的最佳时机III.py
+++ python/123.买卖股票的最佳时机III.py
@@ -1,14 +1,35 @@
 class Solution:
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     days = len(prices)
+    #     max_k = 2
+    #
+    #     # dp[day][k][state]  天数，交易次数   持有/卖出
+    #     dp = [[[0, 0] for _ in range(max_k + 1)] for _ in range(days)]
+    #
+    #     for day in range(days):
+    #         # 遍历交易次数
+    #         for k in range(max_k, 0, -1):
+    #             # base
+    #             if day == 0:
+    #                 dp[0][k][0] = 0
+    #                 dp[0][k][1] = -prices[day]
+    #                 continue
+    #             # day天 k次 不持有： day-1天k次不持有，  持有卖出
+    #             dp[day][k][0] = max(dp[day - 1][k][0], dp[day - 1][k][1] + prices[day])
+    #             # day天k次 持有: day-1天k次持有， day-1天 k-1次 不持有 买入
+    #             dp[day][k][1] = max(
+    #                 dp[day - 1][k][1], dp[day - 1][k - 1][0] - prices[day]
+    #             )
+    #
+    #     return dp[days - 1][max_k][0]
     def maxProfit(self, prices: List[int]) -> int:
-        max_k, n = 2, len(prices)
-        dp = [ [ [0,0] for _ in range(max_k+1) ] for _ in range(n)]
-        
-        for i in range(n):
-            for k in range(max_k,0,-1):
-                if i -1 == -1:
-                    dp[i][k][0] = 0
-                    dp[i][k][1] = -prices[i]
-                    continue
-                dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
-                dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
-        return dp[n-1][max_k][0]
+        # k为2 ，直接拆解, 内存优化
+        dp_i_1_0, dp_i_1_1 = 0, float('-inf')
+        dp_i_2_0, dp_i_2_1 = 0, float('-inf')
+        for price in prices:
+            dp_i_2_0 = max(dp_i_2_0, dp_i_2_1 + price)
+            dp_i_2_1 = max(dp_i_2_1, dp_i_1_0 - price)
+
+            dp_i_1_0 = max(dp_i_1_0, dp_i_1_1 + price)
+            dp_i_1_1 = max(dp_i_1_1, -price)
+        return dp_i_2_0
